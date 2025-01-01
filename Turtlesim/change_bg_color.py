53ac0aa7d000214805e4541482013ae80f152553
Yuriy Drubinskiy <ydrubins@yahoo.com>
2025-01-01 15:23:26 -0600

    revised call_clear_service function

--- Turtlesim/change_bg_color.py
+++ Turtlesim/change_bg_color.py
@@ -1,28 +1,28 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.parameter import Parameter
+from turtlesim.srv import SetPen
 
-#hrere
-class SetBackgroundColor(Node):
+class TurtleBgColorChanger(Node):
     def __init__(self):
-        super().__init__('set_background_color')
+        super().__init__('turtle_bg_color_changer')
+        self.cli = self.create_client(SetPen, '/turtlesim/set_pen')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = SetPen.Request()
 
-        # Declare the parameter under the /turtlesim namespace
-        self.declare_parameter('/turtlesim/background_b', 100)
-
-        # Set the background_b parameter to 100
-        self.set_parameter(Parameter('/turtlesim/background_b', Parameter.Type.INTEGER, 100))
-
-        self.get_logger().info('Set background_b parameter to 100')
-
+    def send_request(self, r, g, b):
+        self.req.r = r
+        self.req.g = g
+        self.req.b = b
+        self.req.width = 0  # Set width to 0 to only change the background color
+        self.future = self.cli.call_async(self.req)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = SetBackgroundColor()
-    rclpy.spin_once(node)  # Run the node briefly to apply the change
-    node.destroy_node()
+    color_changer = TurtleBgColorChanger()
+    color_changer.send_request(255, 0, 0)  # Change background to red
+    rclpy.spin_until_future_complete(color_changer, color_changer.future)
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
